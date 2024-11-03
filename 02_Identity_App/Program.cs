c617a01dbcd6f5648f2f786c90e97712a1528f80
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-11-03 13:14:27 +0300

    Identity App codeing is done.

---
+++ 02_Identity_App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Identity_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Kimlik *****");
+
+            Console.WriteLine();
+            Console.WriteLine("Nurettin");
+            Console.WriteLine("Kaplan");
+            Console.WriteLine("Topkapı Üniversitesi");
+            Console.WriteLine("İstanbul");
+            Console.WriteLine();
+            Console.WriteLine("***** Kimlik *****");
+
+            Console.Read();
+        }
+    }
+}
