23c7b28adf648daccb3848c051af623d454d5685
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-11-03 13:49:13 +0300

    The rectangle example is done.

---
+++ 04_Finding_Rectangle_Area_And_Perimeter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace _04_Finding_Rectangle_Area_And_Perimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write the code to calculate the area and perimeter of a rectangle with its length and width entered in the code section.
+
+            int width, length, area, perimeter;
+            width = 15;
+            length = 7;
+
+            area = width * length;
+            perimeter = 2 * (area + length);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
+
+            Console.Read();
+        }
+    }
+}
