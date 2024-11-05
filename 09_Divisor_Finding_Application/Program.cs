501137a87d9fe6e60485dcab0fe1f68ce4398b33
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-11-05 12:49:48 +0300

    Divisor-finding application coded.

---
+++ 09_Divisor_Finding_Application/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Divisor_Finding_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Tam Bölenleri Bulma Uygulamasına Hoş Geldiniz!");
+            Console.WriteLine();
+            Console.Write("Sayıyı giriniz: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= num; i++)
+            {
+                if(num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
