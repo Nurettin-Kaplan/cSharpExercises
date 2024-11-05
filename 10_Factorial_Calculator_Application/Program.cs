4120e75511cbfca1f5b2ab3d4906852c559af096
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-11-05 13:38:15 +0300

    Factorial calculator application coded.

---
+++ 10_Factorial_Calculator_Application/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Factorial_Calculator_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, result = 1;
+            Console.WriteLine("Faktoriyel Hesaplama Uygulamasına Hoş Geldiniz!");
+            Console.WriteLine();
+            Console.Write("Sayıyı giriniz: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine("Cevap: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
